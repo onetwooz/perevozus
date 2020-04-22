834c3f5c4328a3988d096675b97247f3152c58fa
onetwooz <63231867+onetwooz@users.noreply.github.com>
2020-04-22 22:34:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,11 @@ bot = telebot.TeleBot('1200662214:AAFkgwEx0M85O6dM_ebyfS3BpEe8eoxwbHQ');
 name = '';
 surname = '';
 age = 0;
+
+@bot.message_handler(commands=["start"])
+def start(message):
+bot.send_message(message.chat.id, 'Добрый день! Я - {1.first_name}, оператор службы доставки.  Что привело вас сегодня к нам?'.format(message.from_user, bot.get_me(), parse_mode="html"))
+
 @bot.message_handler(content_types=['text'])
 def start(message):
     if message.text == '/reg':
